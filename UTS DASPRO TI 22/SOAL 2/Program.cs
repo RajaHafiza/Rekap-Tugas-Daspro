c0e3d45259f8f96cc7d803cbcb3e43da01e9a984
RajaHafiza <120369532+RajaHafiza@users.noreply.github.com>
2022-12-16 22:34:44 +0700

    Add files via upload

---
+++ UTS DASPRO TI 22/SOAL 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace soal2uts
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.Clear();
+            Random rnd=new Random();
+            int rNo=rnd.Next(0,100);
+            bool guessed=false;
+            int guesses=0;
+            while(!guessed){
+                Console.Write("Tebak angka antara 1-100 : ");
+                int Inis=Convert.ToInt32(Console.ReadLine());
+                guesses+=1;
+                if(Inis==rNo){
+                    Console.WriteLine("TEBAKAN ANDA BENAR");
+                    Console.WriteLine("SAMPAI JUMPA");
+                    break;
+                }else if(Inis<rNo){
+                    Console.WriteLine("Kurang tepat. Nilai terlalu rendah.");
+                }else if(Inis>rNo){
+                    Console.WriteLine("Kurang tepat.Nilai terlalu tinggi.");
+                }
+            }
+        }
+    }
+}
+
