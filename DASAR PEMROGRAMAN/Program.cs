63934c450b0be88b47e427929cb0ee181a123d52
RajaHafiza <120369532+RajaHafiza@users.noreply.github.com>
2022-12-16 21:49:16 +0700

    Add files via upload

---
+++ DASAR PEMROGRAMAN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Daspro
+{
+    0 references
+    class Hello
+    {
+        0 references
+        static void Main(string[] args)
+        {
+            Console.WriteLine("anda adalah agen rahasia yang bertugas mendapatkan data dari server...");
+            Console.WriteLine("Akses ke server membutuhkan password yang tidak diketahui");
+            Console.WriteLine("Nama saya Raja Hafiza Ramanda Putra");
+        }
+    }
+}
